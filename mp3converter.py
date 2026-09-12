b4adf41d0e05189c7cd3b29a6e8b56861432fa15
sxmvp <iam.sam.pangilinan@gmail.com>
2026-09-12 14:41:58 -0600

    Clean up and organize mp3converter.py
    Refactor code for better readability and maintainability.

--- mp3converter.py
+++ mp3converter.py
@@ -3,22 +3,15 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.ui import WebDriverWait
-import time
-
-
-
 
 options = Options()
 options.add_experimental_option("detach", True)
-
 driver = webdriver.Chrome(options=options)
 driver.maximize_window()
-
-
 converter = "https://cnvmp3.com/v55"
 
 
-#get the list
+# Get the list
 with open("songs.txt", "r", encoding="utf-8") as file:
     content = file.readlines()
     song_list = []
@@ -28,7 +21,6 @@ with open("songs.txt", "r", encoding="utf-8") as file:
 
 
 for i in song_list:
-    time.sleep(1)
     driver.get(f"https://www.youtube.com/results?search_query={i.replace(' ', '+')}")
 
     wait = WebDriverWait(driver, 60)
@@ -37,32 +29,27 @@ for i in song_list:
         ec.presence_of_element_located((By.CSS_SELECTOR, "a#video-title"))
     )
 
-    # Extract the 'href' attribute (the actual video URL)
+    # Extract the href attribute
     video_url = first_video.get_attribute("href")
 
 
-    #new tab
+    # New tab
     driver.get(converter)
 
 
-    #find the input box
+    # Find the input box
     input_box = wait.until(
         ec.presence_of_element_located((By.ID, 'video-url'))
     )
     input_box.send_keys(video_url)
-    time.sleep(1)
 
-    #submit
+    # Submit
     check = wait.until(
         ec.presence_of_element_located((By.ID, 'convert-button-1'))
     )
     check.click()
 
-    #submit
-    isapa = wait.until(
+    # Wait for the download button to be clickable
+    again = wait.until(
         ec.element_to_be_clickable((By.XPATH, '//*[@id="convert-again-btn"]'))
     )
-    time.sleep(1)
-    isapa.click() 
-
-
